d053e0073a32e4ee17a981c3c84cbdf2a955fcbb
Sarthak Makhija <sarthak.makhija@gmail.com>
2026-06-03 23:42:52 +0530

    Sarthak | Adds check in PrintParser to handle trailing comma

--- src/parser/print.rs
+++ src/parser/print.rs
@@ -32,6 +32,11 @@ impl<'src, 'stream, I: Iterator<Item = LexResult<'src>>> PrintParser<'src, 'stre
         arguments.push(expression);
 
         while expression_parser.stream.maybe_matches(TokenType::Comma) {
+            if let Some(next_token) = expression_parser.stream.peek()? {
+                if next_token.token_type == TokenType::Semicolon {
+                    return Err(ParseError::TrailingComma(next_token.line));
+                }
+            }
             let expression = expression_parser.parse()?;
             arguments.push(expression);
         }
@@ -76,6 +81,53 @@ mod tests {
                 crate::ast::expr::BinaryOperator::Plus,
                 Box::new(Expression::I32(10))
             )]))
+        );
+    }
+
+    #[test]
+    fn parse_print_statement_missing_semicolon() {
+        let lexer = Lexer::new("print name", Keywords::new());
+        let mut stream = ParserStream::new(lexer);
+        let mut parser = PrintParser::new(&mut stream);
+
+        let error = parser.parse().unwrap_err();
+        assert_eq!(error, ParseError::UnexpectedEof);
+    }
+
+    #[test]
+    fn parse_print_statement_trailing_comma() {
+        let lexer = Lexer::new("print name, 42,;", Keywords::new());
+        let mut stream = ParserStream::new(lexer);
+        let mut parser = PrintParser::new(&mut stream);
+
+        let error = parser.parse().unwrap_err();
+        assert_eq!(error, ParseError::TrailingComma(1));
+    }
+
+    #[test]
+    fn parse_print_statement_empty_arguments() {
+        let lexer = Lexer::new("print ;", Keywords::new());
+        let mut stream = ParserStream::new(lexer);
+        let mut parser = PrintParser::new(&mut stream);
+
+        let error = parser.parse().unwrap_err();
+        // Since it expects an expression and finds a Semicolon, it will fail to parse the expression.
+        assert_eq!(
+            error,
+            ParseError::UnsupportedPrefixExpression(TokenType::Semicolon, 1)
+        );
+    }
+
+    #[test]
+    fn parse_print_statement_unexpected_token_instead_of_semicolon() {
+        let lexer = Lexer::new("print name loop", Keywords::new());
+        let mut stream = ParserStream::new(lexer);
+        let mut parser = PrintParser::new(&mut stream);
+
+        let error = parser.parse().unwrap_err();
+        assert_eq!(
+            error,
+            ParseError::UnexpectedTokenType(TokenType::Semicolon, TokenType::Loop, 1)
         );
     }
 }
